f9501948f115cd507dabcf8b9aec32533eb7b936
yterajima <terra@e2esound.com>
2015-12-09 23:09:54 +0900

    Add main Parse method

--- dtf.go
+++ dtf.go
@@ -1,9 +1,27 @@
 package dtf
 
 import (
+	"errors"
 	"time"
 )
 
+// Parse generate time.Time from W3C-DTF string
 func Parse(timeStr string) (time.Time, error) {
-
+	switch true {
+	case IsYear(timeStr):
+		return ParseYear(timeStr)
+	case IsYearAndMonth(timeStr):
+		return ParseYearAndMonth(timeStr)
+	case IsCompleteDate(timeStr):
+		return ParseCompleteDate(timeStr)
+	case IsCompleteDateWithMinutes(timeStr):
+		return ParseCompleteDateWithMinutes(timeStr)
+	case IsCompleteDateWithSeconds(timeStr):
+		return ParseCompleteDateWithSeconds(timeStr)
+	case IsCompleteDateWithFractionOfSecond(timeStr):
+		return ParseCompleteDateWithFractionOfSecond(timeStr)
+	default:
+		var parsedTime time.Time
+		return parsedTime, errors.New("privided string is not W3C-DTF format")
+	}
 }
