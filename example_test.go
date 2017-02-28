c6940daf174392824400dad48048d070a4c10483
Yuya Matsushima <yuyam@fillin-inc.com>
2017-03-01 00:52:15 +0900

    fix test

--- example_test.go
+++ example_test.go
@@ -6,6 +6,6 @@ import (
 
 func ExampleParse() {
 	parsed, _ := Parse("2015-12-09T14:22:30+09:00")
-	fmt.Println(parsed.Format("2006-01-02T15:04:05 MST"))
-	// Output: 2015-12-09T14:22:30 JST
+	fmt.Println(parsed.Format("2006-01-02T15:04:05-07:00"))
+	// Output: 2015-12-09T14:22:30+09:00
 }
